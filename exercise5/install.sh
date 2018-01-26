5c7aa001f1afefdfa49acc4bb89d4aca64988f03
sumit dugar <dugar.sumit1990@gmail.com>
2018-01-26 11:48:07 +0100

    test

--- exercise5/install.sh
+++ exercise5/install.sh
@@ -1,5 +1,8 @@
 sudo apt-get remove docker docker-engine docker.io
-sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+sudo apt-get install apt-transport-https 
+sudo apt-get install ca-certificates 
+sudo apt-get install curl 
+sudo apt-get install software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
